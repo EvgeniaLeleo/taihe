c9ccc0b5648b5874bc90cd8009d16dc7d363e3df
Evgenia Fedorova <fedorova.e@ati.su>
2023-10-16 22:26:44 +0300

    refactor: change formatting at media page

--- src/components/InfoBlock/InfoBlock.jsx
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -8,17 +8,19 @@ const cx = cn.bind(styles);
 
 export const InfoBlock = ({ imgUrl, name = '', text = '', isButton = false, linkUrl = '', linkText = '' }) => {
   return (
-    <div>
-      <div className={cx('image-wrapper')}>
-        <img className={cx('image')} src={imgUrl} alt={name} />
+    <div className={cx('info-block')}>
+      <div>
+        <div className={cx('image-wrapper')}>
+          <img className={cx('image')} src={imgUrl} alt={name} />
+        </div>
+        {!!name && <h2 className={cx('name')}>{name}</h2>}
+        {!!text && <p className={cx('text')}>{text}</p>}
+        {isButton && (
+          <Button theme="secondary" mt={26}>
+            Записаться на занятие
+          </Button>
+        )}
       </div>
-      {!!name && <h2 className={cx('name')}>{name}</h2>}
-      {!!text && <p className={cx('text')}>{text}</p>}
-      {isButton && (
-        <Button theme="secondary" mt={26}>
-          Записаться на занятие
-        </Button>
-      )}
       {!!linkUrl && !!linkText && (
         <a className={cx('link')} href="/" target="_blank" rel="noreferrer">
           {linkText}

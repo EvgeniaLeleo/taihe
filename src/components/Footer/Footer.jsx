0e1de695be8a492dff0a22f8dbb0ba35f03f0996
Evgenia Fedorova <fedorova.e@ati.su>
2023-10-17 21:58:17 +0300

    refactor: change footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,21 +38,23 @@ const Footer = () => {
         </LinkButton>
       </div>
       <div className={cx('footer-bottom')}>
-        <div className={cx('contacts-wrapper')}>
-          <a className={cx('contacts-link')} href="mailto:taiheclub@yandex.ru">
-            taiheclub@yandex.ru
-          </a>
-          <a className={cx('contacts-link')} href="tel:+79652071339">
-            +7 (965) 207-13-39
-          </a>
-        </div>
-        <div className={cx('socials')}>
-          <a className={cx('socials__link')} href={URL.youtube} target="_blank" rel="noreferrer">
-            <img className={cx('socials__image')} src={youtube} alt="YouTube" />
-          </a>
-          <a className={cx('socials__link')} href={URL.telegram} target="_blank" rel="noreferrer">
-            <img className={cx('socials__image')} src={telegram} alt="Telegram" />
-          </a>
+        <div className={cx('footer-bottom__content')}>
+          <div className={cx('contacts-wrapper')}>
+            <a className={cx('contacts-link')} href="mailto:taiheclub@yandex.ru">
+              taiheclub@yandex.ru
+            </a>
+            <a className={cx('contacts-link')} href="tel:+79652071339">
+              +7 (965) 207-13-39
+            </a>
+          </div>
+          <div className={cx('socials')}>
+            <a className={cx('socials__link')} href={URL.youtube} target="_blank" rel="noreferrer">
+              <img className={cx('socials__image')} src={youtube} alt="YouTube" />
+            </a>
+            <a className={cx('socials__link')} href={URL.telegram} target="_blank" rel="noreferrer">
+              <img className={cx('socials__image')} src={telegram} alt="Telegram" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>

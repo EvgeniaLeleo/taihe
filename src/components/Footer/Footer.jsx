88794c389bfd16264f14656c16de4d171a2072ca
Evgenia Fedorova <fedorova.e@ati.su>
2023-10-18 18:55:29 +0300

    refactor: change footer socials link styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,8 @@ import { getStore } from '../../utils/getStore';
 
 import youtube from '../../images/icons/youtube-icon-light.svg';
 import telegram from '../../images/icons/telegram-icon-light.svg';
+import youtubeHover from '../../images/icons/youtube-icon-hover-light.svg';
+import telegramHover from '../../images/icons/telegram-icon-hover-light.svg';
 
 import { URL } from '../../constants';
 import styles from './styles.module.css';
@@ -53,9 +55,11 @@ const Footer = () => {
           <div className={cx('socials')}>
             <a className={cx('socials__link')} href={URL.youtube} target="_blank" rel="noreferrer">
               <img className={cx('socials__image')} src={youtube} alt="YouTube" />
+              <img className={cx('socials__image', 'socials__image_hover')} src={youtubeHover} alt="YouTube" />
             </a>
             <a className={cx('socials__link')} href={URL.telegram} target="_blank" rel="noreferrer">
               <img className={cx('socials__image')} src={telegram} alt="Telegram" />
+              <img className={cx('socials__image', 'socials__image_hover')} src={telegramHover} alt="Telegram" />
             </a>
           </div>
         </div>

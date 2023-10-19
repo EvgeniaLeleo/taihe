3517b1c0411bedf5ff4ed070425c8f25217a488d
Evgenia Fedorova <fedorova.e@ati.su>
2023-10-19 12:31:49 +0300

    refactor: add copy button to card number

--- src/components/CoursesCarousel/CoursesCarousel.jsx
+++ src/components/CoursesCarousel/CoursesCarousel.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Slider from 'react-slick';
 import cn from 'classnames/bind';
 
@@ -10,53 +11,45 @@ import styles from './styles.module.css';
 
 const cx = cn.bind(styles);
 
+const ButtonPrev = ({ setIndex, ...props }) => {
+  const { onClick, currentSlide } = props;
+  const isFirstSlide = currentSlide === 0;
+
+  useEffect(() => {
+    setIndex(currentSlide);
+  }, [currentSlide]);
+
+  return (
+    <button className={cx('button', 'button_left', { button_disabled: isFirstSlide })} onClick={onClick}>
+      <Arrow className={cx('arrow-img', 'arrow-img_left')} />
+    </button>
+  );
+};
+
+const ButtonNext = ({ setIndex, data, ...props }) => {
+  const { onClick, currentSlide } = props;
+  const isLastSlide = data && currentSlide === data.length - 1;
+
+  useEffect(() => {
+    setIndex(currentSlide);
+  }, [currentSlide]);
+
+  return (
+    <button className={cx('button', 'button_right', { button_disabled: isLastSlide })} onClick={onClick}>
+      <Arrow className={cx('arrow-img', 'arrow-img_right')} direction="right" />
+    </button>
+  );
+};
+
 export const CoursesCarousel = ({ data, setIndex }) => {
-  const handleShowPreviousSlide = ({ currentSlide, isFirstSlide }) => {
-    if (!isFirstSlide) {
-      setIndex(currentSlide);
-    }
-  };
-
-  const handleShowNextSlide = ({ currentSlide, isLastSlide }) => {
-    if (!isLastSlide) {
-      setIndex(currentSlide);
-    }
-  };
-
-  const ButtonPrev = (props) => {
-    const { onClick, currentSlide } = props;
-    const isFirstSlide = currentSlide === 0;
-
-    return (
-      <button className={cx('button', 'button_left', { button_disabled: isFirstSlide })} onClick={onClick}>
-        <div className={cx('arrow-img-wrapper')} onClick={handleShowPreviousSlide({ currentSlide, isFirstSlide })}>
-          <Arrow className={cx('arrow-img', 'arrow-img_left')} />
-        </div>
-      </button>
-    );
-  };
-
-  const ButtonNext = (props) => {
-    const { onClick, currentSlide } = props;
-    const isLastSlide = data && currentSlide === data.length - 1;
-
-    return (
-      <button className={cx('button', 'button_right', { button_disabled: isLastSlide })} onClick={onClick}>
-        <div className={cx('arrow-img-wrapper')} onClick={handleShowNextSlide({ currentSlide, isLastSlide })}>
-          <Arrow className={cx('arrow-img', 'arrow-img_right')} direction="right" />
-        </div>
-      </button>
-    );
-  };
-
   return (
     <Slider
       className={cx('carousel')}
       slidesToShow={1}
       slidesToScroll={1}
       infinite={false}
-      nextArrow={<ButtonNext />}
-      prevArrow={<ButtonPrev />}
+      nextArrow={<ButtonNext setIndex={setIndex} data={data} />}
+      prevArrow={<ButtonPrev setIndex={setIndex} />}
     >
       {data.map((item) => (
         <CourseBlock
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import cn from 'classnames/bind';
 
 import { LinkWrapper } from '../../components/LinkWrapper/LinkWrapper';
@@ -14,10 +14,19 @@ const cx = cn.bind(styles);
 export const Contacts = () => {
   const { setIsShopPage } = getStore;
 
+  const [buttonText, setButtonText] = useState('копировать номер карты');
+  const [isCopied, setIsCopied] = useState(false);
+
   useEffect(() => {
     setIsShopPage(false);
     scrollToTop();
   }, []);
+
+  const handleCopy = () => {
+    copyTextToClipboard('2202203513854861');
+    setButtonText('номер скопирован');
+    setIsCopied(true);
+  };
 
   return (
     <div className={cx('contacts-wrapper')}>
@@ -35,16 +44,14 @@ export const Contacts = () => {
 
       <section>
         <h2 className={cx('title')}>Поддержать проекты и автора</h2>
-        <p className={cx('text')}>
-          Сбер:{' '}
-          <button
-            className={cx('button')}
-            onClick={() => copyTextToClipboard('2202203513854861')}
-            title="Копировать номер карты"
-          >
-            2202 2035 1385 4861
-          </button>
-        </p>
+        <div className={cx('text')}>
+          Сбер 2202 2035 1385 4861
+          <div className={cx('button-wrapper', { 'button-wrapper_underlined': !isCopied })}>
+            <button className={cx('button')} onClick={handleCopy}>
+              {buttonText}
+            </button>
+          </div>
+        </div>
         <p className={cx('text')}>Благодарю!</p>
       </section>
     </div>

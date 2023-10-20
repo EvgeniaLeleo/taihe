e40fe5c456b49eadacce588909831def2694bb8e
Evgenia Fedorova <fedorova.e@ati.su>
2023-10-20 21:20:02 +0300

    refactor: change styles

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -29,45 +29,38 @@ export const Courses = () => {
 
   return (
     <MainWrapper>
-      <div className={cx('online-wrapper')}>
-        <section className={cx('section')}>
-          <h2 className={cx('section__title')}>Темы для начального уровня или для знакомства с цигун и тайцзицюань</h2>
+      <section className={cx('section')}>
+        <h2 className={cx('section__title')}>Темы для начального уровня или для знакомства с цигун и тайцзицюань</h2>
 
-          {!!data && (
-            <>
-              <CoursesCarousel data={data.beginner} setIndex={setBeginnerIndex} />
+        {!!data && (
+          <>
+            <CoursesCarousel data={data.beginner} setIndex={setBeginnerIndex} />
 
-              <LinkButton
-                className={cx('link_secondary')}
-                buttonUrl={data.beginner[beginnerIndex].linkUrl}
-                theme="secondary"
-              >
-                {data.beginner[beginnerIndex].linkText || 'Записаться на курс'}
-              </LinkButton>
-            </>
-          )}
-        </section>
+            <LinkButton
+              className={cx('link_secondary')}
+              buttonUrl={data.beginner[beginnerIndex].linkUrl}
+              theme="secondary"
+            >
+              {data.beginner[beginnerIndex].linkText || 'Записаться на курс'}
+            </LinkButton>
+          </>
+        )}
+      </section>
 
-        <section className={cx('section')}>
-          <h2 className={cx('section__title')}>
-            Темы для более опытных занимающихся и для тех, кому интересно глубже изучать методы и технику Хуньюань
-            Тайцзи
-          </h2>
-          {!!data && (
-            <>
-              <CoursesCarousel data={data.master} setIndex={setMasterIndex} />
+      <section className={cx('section')}>
+        <h2 className={cx('section__title')}>
+          Темы для более опытных занимающихся и для тех, кому интересно глубже изучать методы и технику Хуньюань Тайцзи
+        </h2>
+        {!!data && (
+          <>
+            <CoursesCarousel data={data.master} setIndex={setMasterIndex} />
 
-              <LinkButton
-                className={cx('link_secondary')}
-                buttonUrl={data.master[masterIndex].linkUrl}
-                theme="secondary"
-              >
-                {data.master[masterIndex].linkText || 'Записаться на курс'}
-              </LinkButton>
-            </>
-          )}
-        </section>
-      </div>
+            <LinkButton className={cx('link_secondary')} buttonUrl={data.master[masterIndex].linkUrl} theme="secondary">
+              {data.master[masterIndex].linkText || 'Записаться на курс'}
+            </LinkButton>
+          </>
+        )}
+      </section>
     </MainWrapper>
   );
 };
